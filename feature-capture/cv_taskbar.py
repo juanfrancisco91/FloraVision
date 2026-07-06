6338f853a054ab678e4a98b2bd99b771a2403889
juanfrancisco91 <juanfranciso9110@gmail.com>
2026-07-06 16:47:59 -0400

    Calculo agregado

--- feature-capture/cv_taskbar.py
+++ feature-capture/cv_taskbar.py
@@ -8,14 +8,15 @@ def nada(x):
 
 cv.namedWindow('Controles')
 cv.resizeWindow('Controles', 400,300)
-        
+area_total = 0       
 # Creacion de barras de desplazamiento
 cv.createTrackbar('H Min', 'Controles', 0,179, nada)
-cv.createTrackbar('S Min', 'Controles', 0,255, nada)
-cv.createTrackbar('V Min', 'Controles', 0,255, nada)
+cv.createTrackbar('S Min', 'Controles', 50,255, nada)
+cv.createTrackbar('V Min', 'Controles', 50,255, nada)
 cv.createTrackbar('H Max', 'Controles', 179,179, nada)
 cv.createTrackbar('S Max', 'Controles', 255,255, nada)
 cv.createTrackbar('V Max', 'Controles', 255,255, nada)
+#cv.createTrackbar('Tresh Values', 'Controles',0,255, nada)
 
 
 cam = cv.VideoCapture(0, cv.CAP_MSMF)
@@ -24,7 +25,7 @@ while True:
     ret,frame = cam.read()
 
     hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
-
+    gris = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
         
     h_min = cv.getTrackbarPos('H Min', 'Controles')
     s_min = cv.getTrackbarPos('S Min', 'Controles')
@@ -32,6 +33,7 @@ while True:
     h_max = cv.getTrackbarPos('H Max', 'Controles')
     s_max = cv.getTrackbarPos('S Max', 'Controles')
     v_max = cv.getTrackbarPos('V Max', 'Controles')
+    #t_value = cv.getTrackbarPos('Tresh Values', 'Controles')
 
     bajo = np.array([h_min, s_min, v_min])
     alto  = np.array([h_max,s_max,v_max])
@@ -39,9 +41,31 @@ while True:
     mascara = cv.inRange(hsv, bajo, alto)
     resultado = cv.bitwise_and(frame, frame, mask=mascara)
 
+    gris_canny = cv.Canny(resultado, 30, 200)
+
+    _, gris_binaria = cv.threshold(gris_canny,60,255, cv.THRESH_BINARY)        
+    contornos_g, jerarquia = cv.findContours(gris_binaria, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+    mascara_gris = cv.drawContours(resultado, contornos_g, -1, (0,255,0),1)
+
+    
+    umbral = 50
+
+    area = frame.copy()
+    for c in contornos_g:
+        area = cv.contourArea(c)
+        
+        if area > umbral:
+            area_total += area
+            cv.drawContours(resultado, [c], -1,(0,0,255),2)
+    
+
     cv.imshow('Mascara', mascara)
     cv.imshow('Resultado Filtrado', resultado)
     cv.imshow('Original', frame)
+    cv.imshow('gris_canny', gris_canny)
+    
+    print(f'Area total afectada: {area_total}')
+
     print(f"bajo = {h_min}, {s_min}, {v_min}")
     print(f"alto = {h_max}, {s_max}, {v_max}")
 
