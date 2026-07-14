c587227355485704b7aadc9ab1bc6757ca256d94
juanfrancisco91 <juanfranciso9110@gmail.com>
2026-07-13 22:25:34 -0400

    Casi completo deteccion

--- feature-capture/cv_taskbar.py
+++ feature-capture/cv_taskbar.py
@@ -17,6 +17,7 @@ cv.createTrackbar('H Max', 'Controles', 179,179, nada)
 cv.createTrackbar('S Max', 'Controles', 255,255, nada)
 cv.createTrackbar('V Max', 'Controles', 255,255, nada)
 #cv.createTrackbar('Tresh Values', 'Controles',0,255, nada)
+cv.createTrackbar('Preset Rojo', 'Controles', 0, 1, nada)
 
 
 cam = cv.VideoCapture(0, cv.CAP_MSMF)
@@ -25,14 +26,23 @@ img = os.path.join(script,'imagenes_prueba\girasol.jpg')
 img = cv.imread(img)
 while True:
 
-    #ret,frame = cam.read()
+    ret,frame = cam.read()
     
-    frame_total = img
-    frame_danio = img
+    frame_total = frame.copy()
+    frame_danio = frame.copy()
 
-    hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV)
-    gris = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-        
+    hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
+    gris = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    
+    if cv.getTrackbarPos('Preset Rojo', 'Controles') == 1:
+        # Movemos los sliders automáticamente a los extremos
+        cv.setTrackbarPos('H Min', 'Controles', 170)
+        cv.setTrackbarPos('H Max', 'Controles', 10)
+        cv.setTrackbarPos('S Min', 'Controles', 50)  
+        cv.setTrackbarPos('V Min', 'Controles', 50)
+        # Devolvemos el interruptor a 0 inmediatamente
+        cv.setTrackbarPos('Preset Rojo', 'Controles', 0)
+
     h_min = cv.getTrackbarPos('H Min', 'Controles')
     s_min = cv.getTrackbarPos('S Min', 'Controles')
     v_min = cv.getTrackbarPos('V Min', 'Controles')
@@ -41,21 +51,36 @@ while True:
     v_max = cv.getTrackbarPos('V Max', 'Controles')
     #t_value = cv.getTrackbarPos('Tresh Values', 'Controles')
 
-    bajo = np.array([h_min, s_min, v_min])
-    alto  = np.array([h_max,s_max,v_max])
+    if h_min <= h_max:
 
-    mascara_sana = cv.inRange(hsv, bajo, alto)
-    resultado = cv.bitwise_and(img, img, mask=mascara_sana)
+        bajo = np.array([h_min, s_min, v_min])
+        alto = np.array([h_max, s_max, v_max])
+        mascara_sana = cv.inRange(hsv, bajo, alto)
+    else:
+        # MODO DUAL (Rojo: cuando el Mínimo cruza al Máximo)
+        rojo_bajo_1 = np.array([0, s_min, v_min])
+        rojo_alto_1 = np.array([h_max, s_max, v_max])
+        rojo_bajo_2 = np.array([h_min, s_min, v_min])
+        rojo_alto_2 = np.array([179, s_max, v_max])
+        
+        mascara_rojo_1 = cv.inRange(hsv, rojo_bajo_1, rojo_alto_1)
+        mascara_rojo_2 = cv.inRange(hsv, rojo_bajo_2, rojo_alto_2)
+        mascara_sana = cv.bitwise_or(mascara_rojo_1, mascara_rojo_2)
+
+
+    #mascara_sana = cv.inRange(hsv, bajo, alto)
+    resultado = cv.bitwise_and(frame, frame, mask=mascara_sana)
+
 
     H,S,V = cv.split(hsv)
-    v_suave = cv.GaussianBlur(V, (7,7),0)
+    s_suave = cv.GaussianBlur(S, (7,7),0)
     
-    _, gris_nueva = cv.threshold(v_suave, 0,255, cv.THRESH_BINARY + cv.THRESH_OTSU)
+    _, gris_nueva = cv.threshold(s_suave, 20,255, cv.THRESH_BINARY)
 
 
     #gris_canny = cv.Canny(resultado, 30, 200)
 
-    kernel = np.ones((5,5), np.uint8)
+    kernel = np.ones((7,7), np.uint8)
     gris_nueva = cv.morphologyEx(gris_nueva, cv.MORPH_CLOSE, kernel)
     
 
@@ -63,17 +88,25 @@ while True:
     #contornos_g, jerarquia = cv.findContours(gris_binaria, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
     #mascara_gris = cv.drawContours(resultado, contornos_g, -1, (0,255,0),1)
     
-    
     mascara_marchita = cv.bitwise_and(gris_nueva, cv.bitwise_not(mascara_sana))
 
     contornos_total, _  = cv.findContours(gris_nueva, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+    area_total = sum([cv.contourArea(c) for c in contornos_total if cv.contourArea(c) > 100])
+
+
     contornos_marchito, _  = cv.findContours(mascara_marchita, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+    area_marchita = sum([cv.contourArea(c) for c in contornos_marchito if cv.contourArea(c) > 30])
+
+    if area_total > 0:
+        porcentaje_marchito = (area_marchita / area_total) * 100
+    else:
+        porcentaje_marchito = 0
 
     #Funcion para utilizar mas tarde
     #cv.putText(frame, f"Danio {porcentaje_marchito:.1f}", (10,40,)
 
     cv.drawContours(frame_total, contornos_total, -1, (0, 255, 0), 2)
-    cv.drawContours(frame_danio, contornos_marchito, -1, (255, 0, 0), 2)
+    cv.drawContours(frame_danio, contornos_marchito, -1, (0, 0, 255), 2)
     
     #cv.imshow('Mascara', mascara_sana)
     #cv.imshow('Flor Completa', resultado)
@@ -81,16 +114,19 @@ while True:
     #cv.imshow('gris_canny', gris_canny)
     #cv.imshow('Nueva Gris', gris_nueva)
 
-    cv.imshow('1. Mascara Sana (Sliders)', mascara_sana)
-    cv.imshow('2. Molde Flor Completa (Otsu)', frame_total)
-    cv.imshow('3. Marchitamiento Detectado', frame_danio)
+    cv.putText(frame_danio, f'Danio: {porcentaje_marchito:.1f}%', (20,40),
+               cv.FONT_HERSHEY_COMPLEX,1.0,(0,0,255),2)
     
+    cv.imshow('1. Original', frame)
+    cv.imshow('2. Parte Sana Aislada', resultado)
+    cv.imshow('3. Molde Flor Completa', frame_total)
+    cv.imshow('4. Marchitamiento Detectado', frame_danio)
     #print(f'Area total afectada: {area_total}')
 
     #print(f"bajo = {h_min}, {s_min}, {v_min}")
     #print(f"alto = {h_max}, {s_max}, {v_max}")
 
-    if cv.waitKey(0) & 0xFF == 27:
+    if cv.waitKey(1) & 0xFF == 27:
         print(f"Rango encontrado:")
         print(f"bajo = {h_min}, {s_min}, {v_min}")
         print(f"alto = {h_max}, {s_max}, {v_max}")
